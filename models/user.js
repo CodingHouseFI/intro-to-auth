e7b6adc28cb42cfafd10315dcb5212154abb46fe
Cade Nichols <cade@cadenichols.com>
2016-05-06 14:13:29 -0700

    wip middleware

--- models/user.js
+++ models/user.js
@@ -1,17 +1,57 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
+
+// IT'S MIDDLEWARE!!
+userSchema.statics.isLoggedIn = function(req, res, next) {
+
+  // get the token from the cookie
+  // verify the token
+
+  // if the token is bad, respond with error code 401 (hang up)
+  // if the token is good, call next()
+
+};
+
+
 userSchema.statics.register = function(userObj, cb) {
   // create a new user!
   this.create(userObj, cb);
 };
 
+userSchema.statics.authenticate = function(userObj, cb) {
+  // find the user by the username
+  // confirm the password
+
+  // if user is found, and password is good, create a token
+  this.findOne({username: userObj.username}, (err, dbUser) => {
+    if(err || !dbUser) return cb(err || { error: 'Login failed. Username or password incorrect.' });
+
+    if(dbUser.password !== userObj.password) {
+      return cb({error: 'Login failed. Username or password incorrect.'});
+    }
+
+    var token = dbUser.makeToken();
+
+    cb(null, token);
+  });
+};
+
+userSchema.methods.makeToken = function() {
+  var token = jwt.sign({ _id: this._id }, JWT_SECRET);
+  return token;
+};
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var User = require('../models/user');
 
-
 router.get('/', (req, res) => {
   User.find({}, (err, users) => {
     res.status(err ? 400 : 200).send(err || users);
@@ -13,27 +12,22 @@ router.get('/', (req, res) => {
 //   /api/users/register
 router.post('/register', (req, res) => {
   User.register(req.body, err => {
-    res.cookie('cadecookie', 'yup. cookie.');
     res.status(err ? 400 : 200).send(err);
   });
 });
 
 
-// //   /api/users/login
+router.post('/authenticate', (req, res) => {
+  User.authenticate(req.body, (err, token) => {
+    if(err) return res.status(400).send(err);
 
-// router.post('/login', (req, res) => {
+    res.cookie('accessToken', token).send(token);
 
+  });
+});
 
-
-//   if(loginIsValid()) {
-
-//     res.cookie('accessToken', {username: 'mrbob'})
-
-//   }
-
+// router.get('/profile', User.isLoggedIn, (req, res) => {
 
 // })
-
-
 
 module.exports = router;
